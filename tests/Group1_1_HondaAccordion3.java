70d7b7e44e9cbda1080e83614c951a9312594a82
Christina Tashji <christinast2020@gmail.com>
2022-03-18 08:43:01 -0400

    h

---
+++ tests/Group1_1_HondaAccordion3.java
@@ -0,0 +1,12 @@
+package tests;
+
+import bcatest.BCATestScenario;
+
+public class Group1_1_HondaAccordion3 extends BCATestScenario{
+
+    @Override
+    public int runTest() {
+        HondaAccordian a = new HondaAccordian(2018);
+        return 0;
+    }
+}
