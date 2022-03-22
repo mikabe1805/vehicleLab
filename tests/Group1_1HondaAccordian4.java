09de42bd993d5c9db9762f7590446d9073a94dcb
Christina Tashji <christinast2020@gmail.com>
2022-03-22 08:32:37 -0400

    scenario 4 trying to fix

--- tests/Group1_1HondaAccordian4.java
+++ tests/Group1_1HondaAccordian4.java
@@ -13,23 +13,24 @@ public class Group1_1HondaAccordian4 extends BCATestScenario{
     public int runTest() {
         HondaAccordian h = new HondaAccordian(2.0, 2018);
 
-        assertEquals(h.getMake(), "Honda Accordian", "Make should be Honda Accordian");
+        assertEquals(h.getMake(), "Honda", "Make should be Honda");
 
-        assertEquals(h.getModel(), "2018", "Model should be 2018");
+        assertEquals(h.getModel(), "Accordian", "Model should be Accordian");
 
         assertEquals(h.getMileage(), 2.0, .1, "Mileage is 2.0");
 
-        h.drive(30);
+        //h.refillTank();
+        h.drive(30.0);
         assertEquals(h.getMileage(), 32.0, .1, "Mileage should be 32 after driving 30 miles");
 
-        assertEquals(h.toString(), "Honda Accordian 2018 (32.0 mi)", "toString should return Honda Accordian 2018 (32.0 mi)");
+        assertEquals(h.toString(), "2018 Honda Accordian (32.0 mi)", "toString should return 2018 Honda Accordian (32.0 mi)");
 
         assertEquals(h.getRemainingRange(), 4.4, .1, "Remaining range is 4.4");
 
         List<Double> list = List.of(20.2, 20.0, 20.0);
         assertEquals(h.roadTrip(list), 48, .1, "# of days in driving will be 48 days.");
 
-        assertEquals(h.toString(), "Honda Accordian 2018 (90.2 mi)", "toString should return Honda Accordian 2018 (90.2 mi");
+        assertEquals(h.toString(), "2018 Honda Accordian (90.2 mi)", "toString should return 2018 Honda Accordian (90.2 mi)");
 
         List<Double> list2 = List.of(20.2, -20.0, 20.0);
         assertThrows(IllegalArgumentException.class, () -> {h.roadTrip(list2);}, "Mileage cannot be negative.");
